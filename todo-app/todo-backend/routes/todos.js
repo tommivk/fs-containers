592370b1004b248a8442bff77efbb2313e25ee64
tommivk <tommikokk@gmail.com>
2026-04-19 18:07:49 +0300

    implement missing routes

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,14 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+ const data = req.body;
+  await Todo.updateOne({ _id: req.todo.id }, { $set: data })
+  res.sendStatus(200)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
